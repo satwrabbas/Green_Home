4d9df9db2a145d2e92c9e0e6a36809ca8eb85d92
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-01-30 00:42:23 +0300

    fix mouse

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
-import { Tajawal } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
-const tajawal = Tajawal({
-  subsets: ["arabic"],
-  weight: ["200", "400", "500", "700", "800"],
-  variable: "--font-tajawal",
-});
 
 export const metadata: Metadata = {
   title: "Our Home | مكتب هندسي عقاري",
@@ -27,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <body
-        className={`${tajawal.className} antialiased bg-slate-950 text-slate-50`}
+        className={` antialiased bg-slate-950 text-slate-50`}
       >
         <Navbar />
 
